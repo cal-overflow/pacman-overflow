202a49bf32d857e624924576a643f6abc68e16fc
Christian Lisle <lisleachristian@gmail.com>
2022-04-05 01:03:38 -0500

    Created basic Item object

---
+++ src/frontend/utilities/Items/Item.js
@@ -0,0 +1,18 @@
+export default class Item {
+  constructor({ position, points, lifespan, size }) {
+    this.position = position;
+    this.points = points;
+    this.lifespan = lifespan;
+    this.size = size;
+  }
+
+  // override this function for more advanced items (i.e. fruits)
+  draw(ctx) {
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.stroke();
+  }
+}
