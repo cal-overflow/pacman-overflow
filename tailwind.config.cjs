1490d8d4e16d96d36fe44d6f1932cb56141ee9b2
Christian Lisle <lisleachristian@gmail.com>
2022-07-02 20:18:21 -0500

    Added player colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,19 @@
 module.exports = {
   content: [
-    './src/frontend/**/*.{html,js}'
+    './src/frontend/**/*.html',
+    './src/frontend/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FFFF00',
+        'pacman': '#FFFF00',
+        'blinky': '#FF0000',
+        'pinky': '#FFC0CB',
+        'inky': '#ADD8E6',
+        'clyde': '#FFA500',
+      }
+    },
   },
   plugins: [],
 };
